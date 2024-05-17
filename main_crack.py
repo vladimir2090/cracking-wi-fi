2eab1d4e71334b734810f0e6bb2cd69b94e8bbbd
vladimir2090 <zvladzubkov@gmail.com>
2024-05-17 20:39:46 +0300

    main

---
+++ main_crack.py
@@ -0,0 +1,17 @@
+import subprocess
+
+try:
+    data = subprocess.check_output("netsh wlan show profiles").decode('cp866').split('\n')
+    profiles = [i.split(":")[1][1:-1] for i in data if "All user profiles" in i]
+    pass_wifi = ''
+
+    for i in profiles:
+        results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('cp866').split('\n')
+
+        for j in results:
+            if "Key Contents" in j:
+                pass_wifi += f"{i} -- {j.split(':')[1][1:-1]}\n"
+
+    print(pass_wifi)
+except Exception as ex:
+    print(f'Error: {ex}')
